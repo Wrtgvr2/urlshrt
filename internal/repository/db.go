2607dcd220dc160332808e68d2535e3f08c20afd
Wrtgvr <saldaniil123@gmail.com>
2025-04-17 16:24:54 +0700

    Added: DB init

---
+++ internal/repository/db.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDatabase() *gorm.DB {
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.AutoMigrate(&models_db.User{}, &models_db.URL{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
