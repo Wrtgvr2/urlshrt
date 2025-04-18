3fa50741b4dd05e1c3e6207e00399c119bfad254
Wrtgvr <saldaniil123@gmail.com>
2025-04-18 19:32:17 +0700

    Changed: 'Password' field is 'PasswordHash' now

--- internal/models/db/user.go
+++ internal/models/db/user.go
@@ -4,6 +4,6 @@ type User struct {
 	ID              uint64 `gorm:"primaryKey"`
 	Username        string `gorm:"unique;not null;size:30"`
 	DisplayUsername string `gorm:"size:30"`
-	Password        string `gorm:"size:60;not null"`
+	PasswordHash    string `gorm:"size:60;not null"`
 	URLs            []URL  `gorm:"foreignKey:UserID"`
 }
