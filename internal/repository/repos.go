a309b5674cd1a49477f93cfd1a848e2c95792828
Wrtgvr <saldaniil123@gmail.com>
2025-04-19 16:38:08 +0700

    Minod change

--- internal/repository/repos.go
+++ internal/repository/repos.go
@@ -1,9 +1,12 @@
 package repository
 
-import models_db "github.com/wrtgvr/urlshrt/internal/models/db"
+import (
+	"github.com/wrtgvr/urlshrt/internal/apperrors"
+	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
+)
 
 type UserRepo interface {
-	GetUserByUsername(string) (*models_db.User, error)
+	GetUserByUsername(string) (*models_db.User, *apperrors.AppError)
 }
 
 type UrlRepo interface{}
