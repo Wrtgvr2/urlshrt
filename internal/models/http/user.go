a3519e97288025d582eafead57f7ab7b9e4956a1
Wrtgvr <saldaniil123@gmail.com>
2025-04-22 15:09:26 +0700

    Added: required tag on password field. Removed: regexp tag from password tag (I wont make custom validator, I'll do easier)

--- internal/models/http/user.go
+++ internal/models/http/user.go
@@ -3,7 +3,7 @@ package models_http
 type UserRequest struct {
 	Username        string  `json:"username" binding:"required,alphanum,min=5,max=30"`
 	DisplayUsername *string `json:"displayusername" binding:"omitempty"`
-	Password        string  `json:"password" binding:"min=8,max=30,regexp=^[a-zA-Z0-9@#$%^&*!]+$"`
+	Password        string  `json:"password" binding:"required,min=8,max=30"`
 }
 
 type UserResponse struct {
