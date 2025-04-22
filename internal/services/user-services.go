d465285a0deab8a33fc625eeccbef23a970b2b89
Wrtgvr <saldaniil123@gmail.com>
2025-04-22 12:01:16 +0700

    Added: register func

--- internal/services/user-services.go
+++ internal/services/user-services.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
 	models_http "github.com/wrtgvr/urlshrt/internal/models/http"
 	"github.com/wrtgvr/urlshrt/internal/pkg/hash"
 	"github.com/wrtgvr/urlshrt/internal/pkg/jwt"
@@ -16,7 +17,7 @@ func NewUserServices(repo rep.UserRepo) UserServices {
 	return UserServices{Repo: repo}
 }
 
-func (s *UserServices) Login(userReq models_http.UserRequest) (string, string, *errsuit.AppError) {
+func (s *UserServices) Login(userReq *models_http.UserRequest) (string, string, *errsuit.AppError) {
 	user, appErr := s.Repo.GetUserByUsername(userReq.Username)
 	if appErr != nil {
 		return "", "", appErr
@@ -29,4 +30,26 @@ func (s *UserServices) Login(userReq models_http.UserRequest) (string, string, *
 		return "", "", errsuit.NewInternal("can't create tokens", err, true)
 	}
 	return accessToken, refreshToken, nil
+}
+
+func (s *UserServices) Register(userReq *models_http.UserRequest) (*models_db.User, *errsuit.AppError) {
+	user, appErr := s.Repo.GetUserByUsername(userReq.Username)
+	if appErr != nil {
+		return nil, appErr
+	}
+	if user != nil {
+		return nil, errsuit.NewConflict("username is already taken", nil, false)
+	}
+
+	userData, appErr := convertUserReqToUserDb(userReq)
+	if appErr != nil {
+		return nil, appErr
+	}
+
+	createdUser, appErr := s.Repo.CreateUser(userData)
+	if appErr != nil {
+		return nil, appErr
+	}
+
+	return createdUser, nil
 }
