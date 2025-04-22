e05c44e0c7e555c49265235a29946fdd78ff8da2
Wrtgvr <saldaniil123@gmail.com>
2025-04-22 11:17:59 +0700

    Added: validation.v10 validation in UserRequest struct

--- internal/models/http/user.go
+++ internal/models/http/user.go
@@ -1,9 +1,9 @@
 package models_http
 
 type UserRequest struct {
-	Username        string  `json:"username"`
-	DisplayUsername *string `json:"displayusername"`
-	Password        string  `json:"password"`
+	Username        string  `json:"username" binding:"required,alphanum,min=5,max=30"`
+	DisplayUsername *string `json:"displayusername" binding:"omitempty"`
+	Password        string  `json:"password" binding:"min=8,max=30,regexp=^[a-zA-Z0-9@#$%^&*!]+$"`
 }
 
 type UserResponse struct {
