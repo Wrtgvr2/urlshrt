ebf2621d27301547e2c90d392239c9e33b772bad
Wrtgvr <saldaniil123@gmail.com>
2025-04-22 11:17:12 +0700

    Added: empty registerUserRoutes

--- api/router/users.go
+++ api/router/users.go
@@ -1,15 +1,13 @@
 package router
 
-// import (
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/wrtgvr/urlshrt/internal/handlers"
-// )
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wrtgvr/urlshrt/api/handlers"
+)
 
-// func registerUserUrls(h *handlers.Handler, r *gin.Engine) {
-// 	group := r.Group("auth")
-// 	{
-// 		users.GET("/:id", h.getUserHandler)
-// 		users.PUT("/:id", h.updateUserHandler)
-// 		users.DELETE("/:id", h.deleteUserHandler)
-// 	}
-// }
+func registerUserRouter(h *handlers.Handler, r *gin.Engine) {
+	// group := r.Group("users")
+	// {
+	// 	group.GET(/:id, h.GetUserHandler)
+	// }
+}
