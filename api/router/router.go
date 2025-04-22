08dafd1e380518d08314b9f9f7d180f44d03f7d6
Wrtgvr <saldaniil123@gmail.com>
2025-04-22 11:13:50 +0700

    Added: registerUserRouter. && small fix

--- api/router/router.go
+++ api/router/router.go
@@ -2,13 +2,14 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/wrtgvr/urlshrt/internal/handlers"
+	"github.com/wrtgvr/urlshrt/api/handlers"
 )
 
 func RegisterRoutes(h *handlers.Handler) *gin.Engine {
 	r := gin.Default()
 
-	registerShortUrls(h, r)
+	registerShortUrlsRouter(h, r)
+	registerUserRouter(h, r)
 
 	return r
 }
