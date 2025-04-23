fe96f9a3ea55853f0d53161bd4805e2aef2fdc34
Wrtgvr <saldaniil123@gmail.com>
2025-04-23 20:57:59 +0700

    Small code refactoring

--- internal/repository/db-users.go
+++ internal/repository/db-users.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+
 	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
 	"github.com/wrtgvr2/errsuit"
 	"gorm.io/gorm"
@@ -14,9 +16,9 @@ func NewPostgresUserRepo(db *gorm.DB) *PostgresUserRepo {
 	return &PostgresUserRepo{DB: db}
 }
 
-func (p *PostgresUserRepo) GetUserByUsername(username string) (*models_db.User, *errsuit.AppError) {
+func (p *PostgresUserRepo) getUser(searchField string, fieldValue any) (*models_db.User, *errsuit.AppError) {
 	var user models_db.User
-	res := p.DB.First(&user, "username = ?", username)
+	res := p.DB.First(&user, fmt.Sprintf("%s = ?", searchField), fieldValue)
 	if res.Error != nil {
 		if res.Error == gorm.ErrRecordNotFound {
 			return nil, errsuit.NewNotFound("user not found", res.Error, false)
@@ -25,6 +27,14 @@ func (p *PostgresUserRepo) GetUserByUsername(username string) (*models_db.User, 
 	}
 
 	return &user, nil
+}
+
+func (p *PostgresUserRepo) GetUserById(id uint64) (*models_db.User, *errsuit.AppError) {
+	return p.getUser("id", id)
+}
+
+func (p *PostgresUserRepo) GetUserByUsername(username string) (*models_db.User, *errsuit.AppError) {
+	return p.getUser("username", username)
 }
 
 func (p *PostgresUserRepo) CreateUser(userData *models_db.User) (*models_db.User, *errsuit.AppError) {
