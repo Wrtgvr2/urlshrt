da39fed381c18609b990c5c2a629723e7f64a41b
Wrtgvr <saldaniil123@gmail.com>
2025-04-24 11:41:29 +0700

    Added: auth middleware

--- api/middleware/auth.go
+++ api/middleware/auth.go
@@ -1,1 +1,31 @@
 package middleware
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wrtgvr/urlshrt/pkg/jwt"
+	"github.com/wrtgvr2/errsuit"
+	"github.com/wrtgvr2/errsuit/drivers/ginadap"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			ginadap.HandleError(c, errsuit.NewUnauthorized("missing authorization header", nil, false))
+			c.Abort()
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		err := jwt.ValidateAccessToken(tokenStr)
+		if err != nil {
+			ginadap.HandleError(c, errsuit.NewUnauthorized("invalid token", err, true))
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
