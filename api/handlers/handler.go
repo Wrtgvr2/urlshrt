5e133c9522b899d2840023edb11466482c605b13
Wrtgvr <saldaniil123@gmail.com>
2025-04-26 18:28:39 +0700

    Updated: handler services

--- api/handlers/handler.go
+++ api/handlers/handler.go
@@ -3,14 +3,17 @@ package handlers
 import "github.com/wrtgvr/urlshrt/internal/services"
 
 type Handler struct {
-	UserServices *services.UserServices
-	UrlServices  *services.UrlServices
-	AuthServices *services.AuthServices
+	UserServices  *services.UserServices
+	UrlServices   *services.UrlServices
+	AuthServices  *services.AuthServices
+	TokenServices *services.TokenServices
 }
 
-func NewHandler(userSer *services.UserServices, urlSer *services.UrlServices, authSer *services.AuthServices) *Handler {
+func NewHandler(userS *services.UserServices, urlS *services.UrlServices, authS *services.AuthServices, tokenS *services.TokenServices) *Handler {
 	return &Handler{
-		UserServices: userSer,
-		UrlServices:  urlSer,
+		userS,
+		urlS,
+		authS,
+		tokenS,
 	}
 }
