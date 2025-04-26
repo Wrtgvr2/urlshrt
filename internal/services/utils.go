49c6503d889a8f2a356f1167adcf57f319dd73d6
Wrtgvr <saldaniil123@gmail.com>
2025-04-26 17:37:17 +0700

    Added: func to create token model with given data

--- internal/services/utils.go
+++ internal/services/utils.go
@@ -1,10 +1,13 @@
 package services
 
 import (
+	"time"
+
 	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
 	models_http "github.com/wrtgvr/urlshrt/internal/models/http"
 	"github.com/wrtgvr/urlshrt/internal/validation"
 	"github.com/wrtgvr/urlshrt/pkg/hash"
+	"github.com/wrtgvr/urlshrt/pkg/jwt"
 	"github.com/wrtgvr2/errsuit"
 )
 
@@ -37,4 +40,24 @@ func ValidateUserData(username, password string) *errsuit.AppError {
 		return err
 	}
 	return nil
+}
+
+func CreateRefreshTokenModel(tokenStr string) (*models_db.RefreshToken, *errsuit.AppError) {
+	tokenJti, err := jwt.GetJtiFromRefreshToken(tokenStr)
+	if err != nil {
+		return nil, errsuit.NewInternal("unable to get jti from token", err, true)
+	}
+	tokenUserId, err := jwt.GetUserIdFromToken(tokenStr)
+	if err != nil {
+		return nil, errsuit.NewInternal("unable to get user ID from token", err, true)
+	}
+	tokenExp, err := jwt.GetTokenExpirationUnixTime(tokenStr)
+	if err != nil {
+		return nil, errsuit.NewInternal("unable to get expiration time from token", err, true)
+	}
+	return &models_db.RefreshToken{
+		JTI:       tokenJti,
+		UserID:    tokenUserId,
+		ExpiresAt: time.Unix(tokenExp, 0),
+	}, nil
 }
