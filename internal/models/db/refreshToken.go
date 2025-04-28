21bc33064b33ff67c24f6f25fbfaa44290ac03c2
Wrtgvr <saldaniil123@gmail.com>
2025-04-28 12:34:10 +0700

    Fixed: models tags

--- internal/models/db/refreshToken.go
+++ internal/models/db/refreshToken.go
@@ -7,8 +7,8 @@ import (
 )
 
 type RefreshToken struct {
-	JTI       uuid.UUID `gorm:"type:uuid;primaryKey;index"`
-	UserID    uint64    `gorm:"type:uuid;primaryKey"`
+	JTI       uuid.UUID `gorm:"nut null;type:uuid;primaryKey;index"`
+	UserID    uint64    `gorm:"not null"`
 	ExpiresAt time.Time `gorm:"not null;index"`
 	Revoked   bool      `gorm:"default:false"`
 	CreatedAt time.Time
