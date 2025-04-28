52c8937e0cbbba19072d6c1a0224420150ad158f
Wrtgvr <saldaniil123@gmail.com>
2025-04-28 12:35:38 +0700

    Added: func for updatign refresh token

--- api/handlers/auth.go
+++ api/handlers/auth.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	models_http "github.com/wrtgvr/urlshrt/internal/models/http"
+	"github.com/wrtgvr/urlshrt/pkg/jwt"
+	"github.com/wrtgvr2/errsuit"
 	"github.com/wrtgvr2/errsuit/drivers/ginadap"
 )
 
@@ -42,5 +44,39 @@ func (h *Handler) RegisterHandler(c *gin.Context) {
 }
 
 func (h *Handler) RefreshTokenHandler(c *gin.Context) {
+	jtiAny, exists := c.Get("JTI")
+	if !exists {
+		ginadap.HandleError(c, errsuit.NewUnauthorized("jti not found in context", nil, false))
+		return
+	}
+	jti, ok := jtiAny.(string)
+	if !ok {
+		return
+	}
 
+	userIdAny, exists := c.Get("UserID")
+	if !exists {
+		ginadap.HandleError(c, errsuit.NewUnauthorized("user id not found in context", nil, false))
+		return
+	}
+	userId, ok := userIdAny.(uint64)
+	if !ok {
+		ginadap.HandleError(c, errsuit.NewInternal("invalid user id type in context", nil, true))
+		return
+	}
+
+	newAccessToken, newRefreshToken, err := jwt.CreateTokens(userId)
+	if ginadap.HandleError(c, err) {
+		return
+	}
+
+	_, err = h.TokenServices.UpdateRefreshToken(jti, newRefreshToken)
+	if ginadap.HandleError(c, err) {
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"access_token":  newAccessToken,
+		"refresh_token": newRefreshToken,
+	})
 }
---
+++ internal/services/token-services.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
+	rep "github.com/wrtgvr/urlshrt/internal/repository"
+	"github.com/wrtgvr2/errsuit"
+)
+
+type TokenServices struct {
+	Repo rep.TokenRepo
+}
+
+func NewTokenServices(repo rep.TokenRepo) TokenServices {
+	return TokenServices{
+		Repo: repo,
+	}
+}
+
+func (s *TokenServices) UpdateRefreshToken(jti, newTokenStr string) (*models_db.RefreshToken, *errsuit.AppError) {
+	_, appErr := s.Repo.GetNotRevokedTokenByJTI(jti)
+	if appErr != nil {
+		return nil, appErr
+	}
+
+	appErr = s.Repo.RevokeToken(jti)
+	if appErr != nil {
+		return nil, appErr
+	}
+
+	tokenModel, appErr := createRefreshTokenModel(newTokenStr)
+	if appErr != nil {
+		return nil, appErr
+	}
+
+	newToken, appErr := s.Repo.CreateRefreshTokenInfo(tokenModel)
+	if appErr != nil {
+		return nil, appErr
+	}
+	return newToken, nil
+}
