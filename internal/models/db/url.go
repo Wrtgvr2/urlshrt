58623a0b57d66ff502e205d1f8eb2d9897680779
Wrtgvr <saldaniil123@gmail.com>
2025-05-03 09:38:58 +0700

    Minor change

--- internal/models/db/url.go
+++ internal/models/db/url.go
@@ -4,8 +4,8 @@ import "time"
 
 type URL struct {
 	ID        uint   `gorm:"primaryKey"`
-	ShortURL  string `gorm:"not null;uniqueIndex"`
 	UserID    uint64 `gorm:"not null;index"`
+	ShortURL  string `gorm:"not null;uniqueIndex"`
 	OrigURL   string `gorm:"type:text;not null"`
 	Revoked   bool   `form:"default:false"`
 	CreatedAt time.Time
