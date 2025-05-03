cf328bb069fa0c3fe9501c1fc854fc01946543b2
Wrtgvr <saldaniil123@gmail.com>
2025-05-03 19:37:14 +0700

    Added: handler for POST method on /api/r/shorten

--- internal/repository/db-urls.go
+++ internal/repository/db-urls.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
+	"github.com/wrtgvr2/errsuit"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +12,12 @@ type PostgresUlrRepo struct {
 
 func NewPostgresUrlRepo(db *gorm.DB) *PostgresUlrRepo {
 	return &PostgresUlrRepo{DB: db}
+}
+
+func (p *PostgresUlrRepo) CreateNewShortUrl(urlModel *models_db.URL) (*models_db.URL, *errsuit.AppError) {
+	res := p.DB.Create(urlModel)
+	if res.Error != nil {
+		return nil, errsuit.NewInternal("unable to create url", res.Error, true)
+	}
+	return urlModel, nil
 }
--- internal/services/url-services.go
+++ internal/services/url-services.go
@@ -1,7 +1,16 @@
 package services
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/jxskiss/base62"
+	models_db "github.com/wrtgvr/urlshrt/internal/models/db"
+	models_http "github.com/wrtgvr/urlshrt/internal/models/http"
 	rep "github.com/wrtgvr/urlshrt/internal/repository"
+	"github.com/wrtgvr2/errsuit"
 )
 
 type UrlServices struct {
@@ -10,4 +19,25 @@ type UrlServices struct {
 
 func NewUrlServices(repo rep.UrlRepo) UrlServices {
 	return UrlServices{Repo: repo}
+}
+
+func (s *UrlServices) CreateNewShortUrl(userId uint64, urlReq models_http.UrlRequest) (*models_http.UrlResponse, *errsuit.AppError) {
+	salt := urlReq.URL + fmt.Sprintf("%d%d%s", userId, time.Now().UnixNano(), os.Getenv("URL_ULTRA_SECRET_EXTRA_SALT"))
+	hash := sha1.Sum([]byte(salt))
+	shortUrl := base62.EncodeToString(hash[:8])
+
+	expirationDate := time.Now().Add(30 * 24 * time.Hour)
+	ulrModel := models_db.URL{
+		UserID:    userId,
+		OrigURL:   urlReq.URL,
+		ShortURL:  shortUrl,
+		ExpiresAt: &expirationDate,
+	}
+
+	createdUrl, err := s.Repo.CreateNewShortUrl(&ulrModel)
+	if err != nil {
+		return nil, err
+	}
+
+	return convertUrlDbToUrlResp(createdUrl), nil
 }
